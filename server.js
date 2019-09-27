1769c9f62fca4863c30190e2f08a0425bad0eb66
haydane <haydane9@gmail.com>
2019-09-27 20:38:37 +0700

    update console

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ io.on('connection', socket => {
     
     socket.on('new-user', name => {
         users[socket.id] = name;
-        socket.broadcast.emit('user-connected',name);
-        console.log(users);
+        socket.broadcast.emit('user-connected',name => {
+            console.log(`${name} connected`);
+        });
     });
     socket.on('send-chat-msg', msg => {
         socket.broadcast.emit('chat-message', {
@@ -18,10 +19,11 @@ io.on('connection', socket => {
     });
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnected',users[socket.id]);
+        console.log(`${users[socket.id]} disconnected`);
         delete users[socket.id];
     })
     socket.on('user-typing', name => {
         socket.broadcast.emit('typing', name);
-        console.log(`${name} is typing`);
+        // console.log(`${name} is typing`);
     })
 });
